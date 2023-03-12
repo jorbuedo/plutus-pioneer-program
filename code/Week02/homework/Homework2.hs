c9d1911e02f995072e83521166036ddf2111722f
jorbuedo <jorbuedo@gmail.com>
2023-03-12 10:11:40 +0000

    Week 2 - Homework pass

--- code/Week02/homework/Homework2.hs
+++ code/Week02/homework/Homework2.hs
@@ -8,10 +8,9 @@
 module Homework2 where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
-import           PlutusTx             (unstableMakeIsData)
-import           PlutusTx.Prelude     (Bool, BuiltinData)
-import           Prelude              (undefined)
---import           Utilities            (wrap)
+import           PlutusTx             (compile,unstableMakeIsData)
+import           PlutusTx.Prelude     (Bool, BuiltinData, (/=))
+import           Utilities            (wrap)
 
 ---------------------------------------------------------------------------------------------------
 ----------------------------------- ON-CHAIN / VALIDATOR ------------------------------------------
@@ -26,10 +25,10 @@ PlutusTx.unstableMakeIsData ''MyRedeemer
 {-# INLINABLE mkValidator #-}
 -- Create a validator that unlocks the funds if MyRedemeer's flags are different
 mkValidator :: () -> MyRedeemer -> PlutusV2.ScriptContext -> Bool
-mkValidator = undefined
+mkValidator _ redeemer _ = flag1 redeemer /= flag2 redeemer
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-wrappedVal = undefined
+wrappedVal = wrap mkValidator
 
 validator :: PlutusV2.Validator
-validator = undefined
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| wrappedVal ||])
